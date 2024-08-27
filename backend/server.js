9e28dc65d507c4fa81013863b43b511261055cf0
kunal-216 <kunaltalreja216@gmail.com>
2024-08-27 19:20:35 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-import express from "express"
-import cors from "cors"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
@@ -11,29 +11,35 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// middleware
+// Middleware
 const corsOptions = {
     origin: process.env.FRONTEND_URI,
     credentials: true,
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
+
+app.use("/api/user", userRouter);
+app.use("/images", express.static('image_uploads'));
+app.use("/resume", express.static('resume_uploads'));
+
+app.use("/", (req, res) => {
+    res.send("Hello from server");
+});
+
+// Handle undefined routes
+app.use((req, res, next) => {
+    res.status(404).send("Sorry, can't find that!");
+});
+
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
-
-app.use("/api/user",userRouter);
-app.use("/images",express.static('image_uploads'))
-app.use("/resume",express.static('resume_uploads'))
-
-app.use("/",(req,res)=>{
-    res.send("Hello from server");
-})
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+});
