4f3eab27879b62dade3fb98f17d43d40ff1ed0a9
kunal-216 <kunaltalreja216@gmail.com>
2024-08-27 18:39:21 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 connectDB();
@@ -12,13 +13,13 @@ const PORT = process.env.PORT || 3000;
 
 // middleware
 const corsOptions = {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: process.env.FRONTEND_URI,
     credentials: true,
 }
 app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use("/api/user",userRouter);
 app.use("/images",express.static('image_uploads'))
